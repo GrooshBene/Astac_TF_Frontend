27f53437a92234d4092ce747df9114c4460f6b1d
박지수 <wltn000129@gmail.com>
2016-09-19 23:22:25 +0900

    PlaceInfoActivity.java Listview Needs to add ListviewAdapter

---
+++ TodaysFeeling/app/src/main/java/grooshbene/edcan/kr/todaysfeeling/ListData.java
@@ -0,0 +1,32 @@
+package grooshbene.edcan.kr.todaysfeeling;
+
+import android.content.Context;
+
+/**
+ * Created by GrooshBene on 2016. 9. 19..
+ */
+public class ListData {
+    public String content_label;
+    public String description;
+    public String phone;
+    public int spCnt;
+
+    public ListData(Context context, String phone_) {
+        phone = phone_;
+    }
+
+
+    public String getContent_label() {
+        return content_label;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public int getSpCnt(){return spCnt;}
+}
--- TodaysFeeling/app/src/main/java/grooshbene/edcan/kr/todaysfeeling/PlaceInfoActivity.java
+++ TodaysFeeling/app/src/main/java/grooshbene/edcan/kr/todaysfeeling/PlaceInfoActivity.java
@@ -3,6 +3,7 @@ package grooshbene.edcan.kr.todaysfeeling;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.content.Intent;
+import android.database.DataSetObserver;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
@@ -11,9 +12,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by bene on 2016. 6. 21..
@@ -26,19 +31,23 @@ public class PlaceInfoActivity extends AppCompatActivity implements View.OnClick
     Toolbar mToolbar;
     ListView mListView;
     LayoutInflater mInflater;
+    ArrayAdapter<String> mAdapter;
+    ArrayList<String> arrList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_placeinfo);
         mInflater = getLayoutInflater();
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrList);
         mCollapsingToolbar = (CollapsingToolbarLayout)findViewById(R.id.collapsing_toolbar);
         mToolbar = (Toolbar)findViewById(R.id.mToolbar);
         mListView = (ListView)findViewById(R.id.mListView);
+        mListView.setAdapter(mAdapter);
         mImageView = (ImageView)findViewById(R.id.image);
         mImageView.setImageResource(R.drawable.eydia_image);
         ViewGroup header = (ViewGroup)mInflater.inflate(R.layout.place_listview_header, mListView, false);
         setSupportActionBar(mToolbar);
-//        mListView.addHeaderView(header);
+        mListView.addHeaderView(header);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
